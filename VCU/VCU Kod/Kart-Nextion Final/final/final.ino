9de56a2f1fabd077af1d4468c17ca3448fe5d9b5
gunalpms <80674364+gunalpms@users.noreply.github.com>
2022-04-03 12:28:47 +0300

    hatalar giderildi, fonksiyonlar eklendi

--- VCU/VCU Kod/Kart-Nextion Final/final/final.ino
+++ VCU/VCU Kod/Kart-Nextion Final/final/final.ino
@@ -1,8 +1,4 @@
-// sol led sol sinyali, sağ led sağ sinyali temsil eder
-// amaç burada nextiondan alınan veriyi digital pinden dışarı vermektir
-// nextion veriyi 4 hex byte şeklinde verir.
-// bu örnekte sadece ilk byte'ı kontrol etmek yeter ancak 
-// aynı mantıkla bütün received_data array'i kontrol edilebilir.
+#include <SoftwareSerial.h>
 
 int LED_SOL = 13; //internal
 int LED_SAG = 6;
@@ -10,22 +6,20 @@ byte received_data[5]; // 5 bytelık yer önceden açmak performansa da yararlı
 
 void setup(){
   Serial.begin(9600);
+  //SoftwareSerial dbgSerial(5, 6); dummy serial
   pinMode(LED_SOL, OUTPUT);
   pinMode(LED_SAG, OUTPUT);
 }
 
-void loop() {
-  if (Serial.available() > 0) //serial girisi varsa
-  {
-    int recieved = Serial.readBytesUntil('\n', received_data, 5); // terminating olarak newline yolluyor nextion
-    int info = received_data[0];
-    int state = received_data[1]; 
-    Serial.print(info); 
-    Serial.print('\n');
-    Serial.print(state);
-    Serial.print('\n'); 
+void sendCmd(String cmd)
+{
+  Serial.print(cmd);
+  Serial.write("\xFF\xFF\xFF"); //3 tane bu 
+                                // byte gelmedikce nextion komut almiyor
+}
 
-    if (info == 97) {
+void parseNextionCmd(int info); {
+  if (info == 97) {
       if (state == 0) {
         Serial.print("Sol sinyal kapandi");
         Serial.print('\n');
@@ -65,12 +59,19 @@ void loop() {
         Serial.print("Dortlu acildi");
         Serial.print('\n');
       }
-    } 
-   }
+    }   
+}
 
-   void nextionSend(String cmd) {
-    Serial.write(cmd);
-    Serial.write("\xFF\xFF\xFF");
-    
-   }
+void loop() {
+  if (Serial.available() > 0) //serial girisi varsa
+  {
+    int recieved = Serial.readBytesUntil('\n', received_data, 5); // terminating olarak newline yolluyor nextion
+    int info = received_data[0];
+    int state = received_data[1]; 
+    Serial.print(info); 
+    Serial.print('\n');
+    Serial.print(state);
+    Serial.print('\n'); 
+    parseNextionCmd(info);
+  }
 }
